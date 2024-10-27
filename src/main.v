a45dcb26058bdab8d3a6f33a74796ca77ab53435
snowflakednew <syahdriandoank@gmail.com>
2024-10-27 20:12:11 +0700

    Add files

---
+++ src/main.v
@@ -0,0 +1,34 @@
+module main
+
+import os
+import term
+import rand
+
+fn main() {
+	mut n := 0
+	mut cobaan := 0
+	mut batas := 0
+	term.clear()
+	println('Level:')
+	for i in 1..21 {
+		n = i * 500
+		print(i)
+		print('. ')
+		println('0-$n')
+	}
+	batas = os.input('Level? ').int() * 500
+	angka := rand.int_in_range(0, batas) or { panic('$err') }
+	term.clear()
+	for {
+		tebakan := os.input('Tebak! ').int()
+		cobaan++
+		match true {
+			tebakan < angka { println('Lebih besar!') }
+			tebakan > angka { println('Lebih kecil!') }
+			tebakan == angka { println(term.green('Benar! menggunakan $cobaan percobaan.')); break }
+			else { println('Input tidak valid.') }
+		}
+	}
+	println('')
+	exit(0)
+}
---
+++ v.mod
@@ -0,0 +1,7 @@
+Module {
+	name: 'tebakangka'
+	description: 'Game menebak angka di bahasa pemrograman V.'
+	version: '1.0.0'
+	license: 'GPLv3'
+	dependencies: []
+}
