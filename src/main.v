6247ad0e7a72a4066d246ef836ccb7c4bdba60c7
prettycoolmat <syahdriandoank@gmail.com>
2024-11-07 21:35:22 +0700

    fmt src/main.v

--- src/main.v
+++ src/main.v
@@ -10,23 +10,32 @@ fn main() {
 	mut batas := 0
 	term.clear()
 	println('Level:')
-	for i in 1..21 {
+	for i in 1 .. 21 {
 		n = i * 500
 		print(i)
 		print('. ')
-		println('0-$n')
+		println('0-${n}')
 	}
 	batas = os.input('Level? ').int() * 500
-	angka := rand.int_in_range(0, batas) or { panic('$err') }
+	angka := rand.int_in_range(0, batas) or { panic('${err}') }
 	term.clear()
 	for {
 		tebakan := os.input('Tebak! ').int()
 		cobaan++
 		match true {
-			tebakan < angka { println('Lebih besar!') }
-			tebakan > angka { println('Lebih kecil!') }
-			tebakan == angka { println(term.green('Benar! menggunakan $cobaan percobaan.')); break }
-			else { println('Input tidak valid.') }
+			tebakan < angka {
+				println('Lebih besar!')
+			}
+			tebakan > angka {
+				println('Lebih kecil!')
+			}
+			tebakan == angka {
+				println(term.green('Benar! menggunakan ${cobaan} percobaan.'))
+				break
+			}
+			else {
+				println('Input tidak valid.')
+			}
 		}
 	}
 	println('')
